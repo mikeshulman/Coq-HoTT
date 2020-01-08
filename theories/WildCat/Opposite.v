aee837394a60fd60a3504a2e040f04d1b33588c7
Emily Riehl <eriehl@math.jhu.edu>
2020-01-08 00:05:12 -0500

    opposites of stuff in core

--- theories/WildCat/Opposite.v
+++ theories/WildCat/Opposite.v
@@ -17,6 +17,8 @@ Notation "A ^op" := (op A).
 (** This stops typeclass search from trying to unfold op. *)
 Typeclasses Opaque op.
 
+(** opposite 0coh1 categories, groupoids, and functors *)
+
 Global Instance is0coh1cat_op A `{Is0Coh1Cat A} : Is0Coh1Cat (A ^op)
   := Build_Is0Coh1Cat A (fun a b => b $-> a) Id (fun a b c g f => f $o g).
 
@@ -35,7 +37,19 @@ Proof.
   exact (fun f => f^$).
 Defined.
 
-Global Instance is0cat2cat_op A `{Is0Coh2Cat A} : Is0Coh2Cat A^op.
+Global Instance is0coh1fun_op  A `{Is0Coh1Cat A} B `{Is0Coh1Cat B} (F : A -> B) {ff : Is0Coh1Functor F} : Is0Coh1Functor (F : A ^op -> B ^op).
+Proof.
+  apply Build_Is0Coh1Functor.
+  unfold op.
+  cbn.
+  intros a b.
+  apply fmap.
+  assumption.
+Defined.
+
+(** opposite 0coh2 categories and functors *)
+
+Global Instance is0coh2cat_op A `{Is0Coh2Cat A} : Is0Coh2Cat A^op.
 Proof.
   srapply Build_Is0Coh2Cat; unfold op in *; cbn in *.
   - intros a b.
@@ -50,9 +64,16 @@ Proof.
     exact (q $o@ p).
 Defined.
 
+Global Instance is0coh2fun_op A B `{Is0Coh2Cat A} `{Is0Coh2Cat B}
+       (F : A -> B) {ff : Is0Coh1Functor F} {pf : Is0Coh2Functor F} : Is0Coh2Functor (F : A^op -> B^op).
+Proof.
+  apply Build_Is0Coh2Functor; unfold op in *; cbn in *.
+  intros a b.
+  apply fmap2.
+  assumption.
+Defined.
 
-(** WORK IN PROGRESS 
-
+(** opposite 1coh1 categories and functors *)
 
 Global Instance is1coh1cat_op A `{Is1Coh1Cat A} : Is1Coh1Cat A^op.
 Proof.
@@ -70,40 +91,12 @@ Proof.
   srapply Build_Is1Coh1Cat_Strong'; unfold op in *; cbn in *.
   - intros a b c d f g h; exact (cat_assoc_opp_strong h g f).
   - intros a b c d f g h; exact (cat_assoc_strong h g f).
-  - intros a b f; exact (cat_idr_strong f).
-  - intros a b f; exact (cat_idl_strong f).
-  - intros a; exact (cat_idlr_strong a).
-Defined.
-
-(* Opposites are definitionally involutive. You can test this by uncommenting the stuff below. *)
-(*
-Definition test1 A {ac : Is0Coh1Cat A} : A = (A^op)^op := 1.
-Definition test2 A {ac : Is0Coh1Cat A} : ac = is0coh1cat_op (A^op) := 1.
-Definition test3 A {ac : Is0Coh1Cat A} {ac2 : Is0Coh2Cat A} : ac2 = is0coh2cat_op (A^op) := 1.
-Definition test4 A {ac : Is0Coh1Cat A} {ac2 : Is0Coh2Cat A} {ac11 : Is1Coh1Cat A} : ac11 = is1coh1cat_op (A^op) := 1.
-*)
-
-(* Opposite functors and natural transformations *)
-
-Global Instance is0coh1fun_op  A `{Is0Coh1Cat A} B `{Is0Coh1Cat B} (F : A -> B) {ff : Is0Coh1Functor F} : Is0Coh1Functor (F : A ^op -> B ^op).
-Proof.
-  apply Build_Is0Coh1Functor.
-  unfold op.
-  cbn.
-  intros a b.
-  apply fmap.
-  exact ff.
-Defined.
-
-Global Instance is0coh2fun_op A B `{Is0Coh2Cat A} `{Is0Coh2Cat B}
-       (F : A -> B) {ff : Is0Coh1Functor F} {pf : Is0Coh2Functor F} : Is0Coh2Functor (F : A^op -> B^op).
-Proof.
-  apply Build_Is0Coh2Functor.
-  unfold op in *.
-  cbn in *.
-  intros a b.
-  apply fmap2.
-  exact pf.
+  - intros a b f.
+    apply cat_idr_strong.
+  - intros a b f. 
+    apply cat_idl_strong.
+  - intros a.
+    apply cat_idlr_strong.
 Defined.
 
 Global Instance is1coh1fun_op A B `{Is0Coh1Cat A} `{Is0Coh1Cat B} `{Is0Coh2Cat B} (F : A -> B) {ff : Is0Coh1Functor F} {pf : Is1Coh1Functor F} : Is1Coh1Functor (F : A^op -> B^op).
@@ -117,7 +110,17 @@ Proof.
     exact pf.
 Defined.
 
-Definition transformation_op {A} {B} `{Is0Coh1Cat B} (F : A -> B) (G : A -> B) (alpha : F $--> G) : (@Transformation (A^op) (B^op) (is0coh1cat_op B) (G : (A^op) -> (B^op)) (F : (A^op) -> (B^op))).
+(* Opposites are definitionally involutive. You can test this by uncommenting the stuff below. *)
+(*
+Definition test1 A {ac : Is0Coh1Cat A} : A = (A^op)^op := 1.
+Definition test2 A {ac : Is0Coh1Cat A} : ac = is0coh1cat_op (A^op) := 1.
+Definition test3 A {ac : Is0Coh1Cat A} {ac2 : Is0Coh2Cat A} : ac2 = is0coh2cat_op (A^op) := 1.
+Definition test4 A {ac : Is0Coh1Cat A} {ac2 : Is0Coh2Cat A} {ac11 : Is1Coh1Cat A} : ac11 = is1coh1cat_op (A^op) := 1.
+*)
+
+(* Opposite natural transformations *)
+
+Definition transformation_op {A} {B} `{Is0Coh1Cat B} (F : A -> B) (G : A -> B) (alpha : F $=> G) : (@Transformation (A^op) (B^op) (is0coh1cat_op B) (G : (A^op) -> (B^op)) (F : (A^op) -> (B^op))).
 Proof.
   unfold op in *.
   cbn in *.
@@ -126,7 +129,7 @@ Proof.
 Defined.
 
 Global Instance is1nat_op A B `{Is0Coh1Cat A} `{Is0Coh2Cat B}
-       (F : A -> B) {ff1 : Is0Coh1Functor F} (G : A -> B) {fg1 : Is0Coh1Functor G} (alpha : F $--> G) {pf : Is1Natural F G alpha} : Is1Natural (G : A^op -> B^op) (F : A^op -> B^op) (transformation_op F G alpha).
+       (F : A -> B) {ff1 : Is0Coh1Functor F} (G : A -> B) {fg1 : Is0Coh1Functor G} (alpha : F $=> G) {pf : Is1Natural F G alpha} : Is1Natural (G : A^op -> B^op) (F : A^op -> B^op) (transformation_op F G alpha).
 Proof.
   apply Build_Is1Natural'.
   - unfold op in *.
@@ -142,17 +145,5 @@ Proof.
     exact pf.
 Defined.
 
-(* Shorter proof of above using Build_Is1Natural. But maybe the longer proof using Build_Is1Natural' is better?
 
-Proof.
-  apply Build_Is1Natural.
-  unfold op in *.
-  cbn in *.
-  intros a b.
-  unfold transformation_op in *.
-  intros f.
-  apply isnat_opp.
-Defined.
-*)
 
-*)
