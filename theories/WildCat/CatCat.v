866bb4ee0778011f034782d11527d89287828a4b
Morgan Opie <opie.morgan@gmail.com>
2020-01-09 17:47:07 -0500

    adding updates to CatCat before pull.

--- theories/WildCat/CatCat.v
+++ theories/WildCat/CatCat.v
@@ -7,6 +7,7 @@ Require Import Basics.Contractible.
 Require Import Basics.Equivalences.
 Require Import WildCat.Core.
 Require Import WildCat.FunctorCat.
+Require Import WildCat.Equiv.
 
 (** ** Wild category of wild categories *)
 
@@ -36,27 +37,40 @@ Proof.
   serapply Build_Is0Coh1Functor.
   intros u v h. cbn in *.  exact (fmap G ( fmap F h)).
   Defined.
+  
+  (** Next: require more coherences to be a WildCat; get that WildCat is itself more structured. *) 
 
 Record WildCat :=P2
 {
   cat_carrier : Type; cat_is0coh1cat : Is0Coh1Cat cat_carrier;
    cat_is0coh2cat : Is0Coh2Cat cat_carrier; 
-  (* cat_is1coh1cat : Is1Coh1Cat cat_carrier*)(** Might want to require to be 1 coherent 1 category *)
-}.
+   cat_is1coh1cat : Is1Coh1Cat cat_carrier}.
 
 Coercion cat_carrier : WildCat >-> Sortclass. 
 
-(* note for morgan: this allows us to consider WildCats as types. 
+(* note for morgan: this allows us to consider WildCats as types. *)
+Global Existing Instance cat_is1coh1cat. 
+Global Existing Instance cat_is0coh2cat.
+Global Existing Instance cat_is0coh1cat.
 
-Global Existing Instance cat_is1coh1cat. *)
-
+(** The proof below is identical to that showing that WildCat01 is a 0 coherent 1 category. This is because every 1 coherent 1 category is a 0 coherent 1 category. *)
 Global Instance is0coh1cat_wildcat : Is0Coh1Cat WildCat.
 Proof.
   serapply Build_Is0Coh1Cat.
   + intros A B.
-Admitted.
+  exact (Fun1 A B).
+  + intro C. unfold Fun1. 
+  exists idmap. exact _.
+  + intros C D E; cbn in *. 
+  intros [F f]. intros [G g].
+  unfold Fun1.
+  exists (F o G). 
+  serapply Build_Is0Coh1Functor.
+  intros a b h. cbn in *. exact (fmap F( fmap G h)).
+  Defined.
 
-(* Need to have a way to take the core of Fun1 A B to get a gropoid so that we can have a 0 coherent 2 category structure on WildCat.*)
+
+(** Need to have a way to take the core of Fun1 A B to get a gropoid so that we can have a 0 coherent 2 category structure on WildCat.*)
 
 Definition core (A : Type) `{h : Is0Coh1Cat A} := A.
 
@@ -70,7 +84,7 @@ Global Instance is0coh1gpd_core (A : Type) `{ h : Is0Coh1Cat(A)} : Is0Coh1Gpd ( 
 Proof. rapply Build_Is0Coh1Gpd.
 Admitted.
 
-(* now show WildCat is 0 coh 2 cat with 0 coh 1 category structure on Fun1 given by taking core f natural transoformations. *) 
+(* now show WildCat is 0 coh 2 cat with 0 coh 1 category structure on Fun02 given by taking core f natural transoformations. *) 
 
 Global Instance is0coh2cat_wildcat : Is0Coh2Cat WildCat.
 Proof.
