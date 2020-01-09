9a8a658119d996ba51d43eb10e967b11a02be084
Morgan Opie <opie.morgan@gmail.com>
2020-01-09 17:13:01 -0500

    CatCat update

--- theories/WildCat/CatCat.v
+++ theories/WildCat/CatCat.v
@@ -10,28 +10,26 @@ Require Import WildCat.FunctorCat.
 
 (** ** Wild category of wild categories *)
 
-Record WildCat :=Pl
+Record WildCat01 :=Pl
 {
-  cat_carrier : Type;
-  cat_is0coh1cat : Is0Coh1Cat cat_carrier;
+  cat01_carrier : Type;
+  cat01_is0coh1cat : Is0Coh1Cat cat01_carrier;
   
   (* TODO: How much should we include here? Plan: do for objects 0 coherent 1 categories; then consider 1 coherent 1 categories.*)
 }.
 
-Coercion cat_carrier : WildCat >-> Sortclass. (* note for morgan: this allows us to consider WildCats as types. *)
+Coercion cat01_carrier : WildCat01 >-> Sortclass. (* note for morgan: this allows us to consider WildCats as types. *)
 
-Global Existing Instance cat_is0coh1cat.
+Global Existing Instance cat01_is0coh1cat.
 
-Global Instance is0coh1cat_wildcat : Is0Coh1Cat WildCat.
+Global Instance is0coh1cat_wildcat01 : Is0Coh1Cat WildCat01.
 Proof.
   serapply Build_Is0Coh1Cat.
   + intros A B.  
   exact (Fun1 A B).
   + intros C. cbn in *.
-  unfold Fun1. 
-  (** This part seems redundant. I'm showing idmap is a 0 coherent 1 functor. maybe done elsewhere? (or should be done somewhere else?). In modifying we will probably want that idmap is 0 coherent 2, 1 coherent 1, etc. *)
-  exists (idmap). serapply Build_Is0Coh1Functor. intros a b. cbn.
-  exact (idmap).
+  exists (idmap). 
+  exact _.
   + intros A B C; cbn in *; unfold Fun1.
   intros [G g] [F f]. 
   exists ( G o F). 
@@ -39,6 +37,59 @@ Proof.
   intros u v h. cbn in *.  exact (fmap G ( fmap F h)).
   Defined.
 
+Record WildCat :=P2
+{
+  cat_carrier : Type; cat_is0coh1cat : Is0Coh1Cat cat_carrier;
+   cat_is0coh2cat : Is0Coh2Cat cat_carrier; 
+  (* cat_is1coh1cat : Is1Coh1Cat cat_carrier*)(** Might want to require to be 1 coherent 1 category *)
+}.
+
+Coercion cat_carrier : WildCat >-> Sortclass. 
+
+(* note for morgan: this allows us to consider WildCats as types. 
+
+Global Existing Instance cat_is1coh1cat. *)
+
+Global Instance is0coh1cat_wildcat : Is0Coh1Cat WildCat.
+Proof.
+  serapply Build_Is0Coh1Cat.
+  + intros A B.
+Admitted.
+
+(* Need to have a way to take the core of Fun1 A B to get a gropoid so that we can have a 0 coherent 2 category structure on WildCat.*)
+
+Definition core (A : Type) `{h : Is0Coh1Cat A} := A.
+
+Global Instance is0coh1cat_core (A : Type) `{ h : Is0Coh1Cat(A)} : Is0Coh1Cat ( core A).
+Proof. serapply Build_Is0Coh1Cat.
+  + intros a b. 
+  
+Admitted.
+
+Global Instance is0coh1gpd_core (A : Type) `{ h : Is0Coh1Cat(A)} : Is0Coh1Gpd ( core A).
+Proof. rapply Build_Is0Coh1Gpd.
+Admitted.
+
+(* now show WildCat is 0 coh 2 cat with 0 coh 1 category structure on Fun1 given by taking core f natural transoformations. *) 
+
 Global Instance is0coh2cat_wildcat : Is0Coh2Cat WildCat.
 Proof.
 Admitted.
+
+Global Instance is1coh1cat_wildcat : Is1Coh1Cat WildCat.
+(** Proof.
+  serapply Build_Is1Coh1Cat.
+  + intros A B C D f g h.
+  exact (Fun1 A B).
+  + intros C. cbn in *.
+  unfold Fun1. 
+  exists (idmap). serapply Build_Is0Coh1Functor. intros a b. cbn.
+  exact (idmap).
+  + intros A B C; cbn in *; unfold Fun1.
+  intros [G g] [F f]. 
+  exists ( G o F). 
+  serapply Build_Is0Coh1Functor.
+  intros u v h. cbn in *.  exact (fmap G ( fmap F h)).
+  Defined. *) Admitted. 
+
+
