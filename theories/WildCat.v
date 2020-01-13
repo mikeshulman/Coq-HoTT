280f6fd62c1b21c4f19a0b9bb8c84004a56bc11d
Mike Shulman <shulman@sandiego.edu>
2020-01-13 11:42:33 -0800

    Basics of wild 1-categories; jww alizter, emilyriehl, mpopie, tslilc

---
+++ theories/WildCat.v
@@ -0,0 +1,8 @@
+Require Export WildCat.Core.
+Require Export WildCat.Equiv.
+Require Export WildCat.Opposite.
+Require Export WildCat.Type.
+Require Export WildCat.FunctorCat.
+Require Export WildCat.Yoneda.
+Require Export WildCat.Prod.
+Require Export WildCat.Sum.
