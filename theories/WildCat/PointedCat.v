c49a91c40931ac6b60ec4a8e33135133b95056cf
Ali Caglayan <alizter@gmail.com>
2020-01-23 22:00:16 +0000

    added some lemmas about zero maps

--- theories/WildCat/PointedCat.v
+++ theories/WildCat/PointedCat.v
@@ -9,3 +9,41 @@ Class IsPointedCat (A : Type) `{Is1Cat A} := {
 
 Definition zero_morphism {A : Type} `{IsPointedCat A} {a b : A} : a $-> b
   := (isinitial_zero_object b).1 $o (isterminal_zero_object a).1.
+
+Declare Scope pointedcat_scope.
+Local Open Scope pointedcat_scope.
+
+Notation "1" := zero_object : pointedcat_scope.
+Notation "0" := zero_morphism : pointedcat_scope.
+
+Section ZeroLaws.
+
+  Context {A : Type} `{IsPointedCat A} {a b c : A}
+    (f : a $-> b) (g : b $-> c).
+
+  Local Arguments zero_morphism {_ _ _ _} _ _.
+
+  Definition pc_zero_source (h : 1 $-> a) : h $== 0
+    := (isinitial_zero_object a).2 h
+      $@ ((isinitial_zero_object a).2 0)^$.
+
+  Definition pc_zero_target (h : a $-> 1) : h $== 0
+    := (isterminal_zero_object a).2 h
+      $@ ((isterminal_zero_object a).2 0)^$.
+
+  Definition comp_f0 : g $o 0 $== zero_morphism a c.
+  Proof.
+    refine ((cat_assoc _ _ _)^$ $@ _).
+    apply cat_prewhisker.
+    apply (isinitial_zero_object c).2.
+  Defined.
+
+  Definition comp_0f : 0 $o f $== zero_morphism a c.
+  Proof.
+    refine (cat_assoc _ _ _ $@ _).
+    apply cat_postwhisker.
+    apply (isterminal_zero_object a).2.
+  Defined.
+
+End ZeroLaws.
+
